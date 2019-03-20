67d66d063c05b1f258eccf3af003756443fb1ae7
Eduardo Jirón <emjironal@gmail.com>
2019-03-20 11:20:11 -0600

    Avance en clase
    Se terminó la interfaz gráfica

--- app/src/main/java/com/example/lab2_reproductor_de_musica/MainActivity.java
+++ app/src/main/java/com/example/lab2_reproductor_de_musica/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.lab2_reproductor_de_musica;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
+    private Field[] trackList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        trackList = R.raw.class.getFields();
+    }
+
+    public void btnListenerPrevious(View view)
+    {
+
+    }
+
+    public void btnListenerPlay(View view)
+    {
+
+    }
+
+    public void btnListenerNext(View view)
+    {
+
     }
 }
